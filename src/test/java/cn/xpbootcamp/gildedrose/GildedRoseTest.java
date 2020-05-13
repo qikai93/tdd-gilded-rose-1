755439176e71ee33e36286ce4a7ad114dca10c30
chenyang.fu <1009058470@qq.com>
2020-05-13 20:43:39 +0800

    tasking 3 : given_a_Sulfuras_and_value_and_sellIn_when_given_today_then_output_value

--- src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
@@ -3,6 +3,8 @@ package cn.xpbootcamp.gildedrose;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static cn.xpbootcamp.gildedrose.GildedRose.GoodType.Sulfuras;
+
 public class GildedRoseTest {
 
     @Test
@@ -27,6 +29,18 @@ public class GildedRoseTest {
         Assert.assertEquals(10,value);
         value = gildedRose.getCurrentValue(12);
         Assert.assertEquals(8,value);
+    }
+
+    @Test
+    public void given_a_Sulfuras_and_value_and_sellIn_when_given_today_then_output_value()
+    {
+        GildedRose gildedRose = new GildedRose();
+        gildedRose.goodType = Sulfuras;
+        gildedRose.Quality = 3;
+        int value = gildedRose.getCurrentValue(5);
+        Assert.assertEquals(3,value);
+        value = gildedRose.getCurrentValue(12);
+        Assert.assertEquals(3,value);
     }
 
 }
